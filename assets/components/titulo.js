b66ba1b796da758d5673e8346fe5b7b70fea4403
ester cubillos <espamalu04@gmail.com>
2023-07-13 11:49:36 -0300

    add commit

--- assets/components/titulo.js
+++ assets/components/titulo.js
@@ -6,8 +6,8 @@ export class Titulo extends LitElement {
       :host {
         display: block;
         color: var(--cor-secundaria, #665e57);
-        text-shadow: 2px 2px 0px 0px rgba(0, 0, 0, 0.25);
-        font-family: Exo;
+        text-shadow: 2px 2px  0px rgba(0, 0, 0, 0.25);
+        font-family: var(--fonte-titulo);
         font-size: 48px;
         font-style: normal;
         font-weight: 700;
